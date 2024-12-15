019c46b78c8596e550dec20e28e35fd150748c15
Madhavan Rangarao <madhavanrangarao@Mac.attlocal.net>
2024-12-15 14:31:11 -0800

    test

--- public/widget.js
+++ public/widget.js
@@ -10,6 +10,13 @@ document.getElementById('try-on-button')?.addEventListener('click', function () 
           <!DOCTYPE html>
           <html lang="en">
           <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TWB4K4NZ');</script>
+<!-- End Google Tag Manager -->
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Popup Widget</title>
@@ -21,18 +28,10 @@ document.getElementById('try-on-button')?.addEventListener('click', function () 
                 background-color: #f9f9f9;
               }
             </style>
-            <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TVFKGP24');</script>
-<!-- End Google Tag Manager -->
-
           </head>
           <body>
-          <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVFKGP24"
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWB4K4NZ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
        	  </body>
